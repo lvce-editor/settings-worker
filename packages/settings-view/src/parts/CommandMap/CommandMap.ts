2603cda692746bccd7f3773d751315b3e056138f
Le Vivilet <levivilet@pm.me>
2026-05-25 22:45:14 +0200

    add fns

--- packages/settings-view/src/parts/CommandMap/CommandMap.ts
+++ packages/settings-view/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,13 @@
 import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
 import { getTabs } from '../GetTabs/GetTabs.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
+import { validateSetting } from '../ValidateSetting/ValidateSetting.ts'
+import { validateSettings } from '../ValidateSettings/ValidateSettings.ts'
 
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'SettingsWorker.getSettingsItems': getSettingItems,
   'SettingsWorker.getTabs': getTabs,
+  'SettingsWorker.validateSetting': validateSetting,
+  'SettingsWorker.validateSettings': validateSettings,
 }
--- packages/settings-view/src/parts/SettingsStates/SettingsStates.ts
+++
@@ -1,4 +0,0 @@
-import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
-import type { SettingsState } from '../SettingsState/SettingsState.ts'
-
-export const { get, getCommandIds, registerCommands, set, wrapCommand, wrapGetter } = ViewletRegistry.create<SettingsState>()
