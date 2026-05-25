e8c47b75bfcc005de36b7c7ec8686b65598642c7
Le Vivilet <levivilet@pm.me>
2026-05-25 22:47:51 +0200

    test

--- packages/settings-worker/test/RenderActions.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals'
-import { renderActions } from '../src/parts/RenderActions/RenderActions.ts'
-
-test('renderActions should return empty array', () => {
-  const result = renderActions()
-  expect(result).toBeDefined()
-})
--- packages/settings-worker/test/RenderEventListeners.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
-
-test('renderEventListeners returns array of DomEventListener objects', () => {
-  const eventListeners = renderEventListeners()
-  expect(eventListeners).toBeDefined()
-})
