95146b3bca44f38351f2ab8fc4801d67addde5fb
Le Vivilet <levivilet@pm.me>
2026-05-25 23:37:08 +0200

    feature: add getSettingsItems2 to commandMap (#6)

--- packages/settings-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/settings-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,4 @@
+import { getSettingItems2 } from '../GetSettingItems2/GetSettingItems2.ts'
 import { getSettingItems } from '../GetSettingItems/GetSettingItems.ts'
 import { getTabs } from '../GetTabs/GetTabs.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
@@ -8,6 +9,7 @@ import { validateSettings } from '../ValidateSettings/ValidateSettings.ts'
 export const commandMap = {
   'Initialize.initialize': Initialize.initialize,
   'SettingsWorker.getSettingsItems': getSettingItems,
+  'SettingsWorker.getSettingsItems2': getSettingItems2,
   'SettingsWorker.getTabs': getTabs,
   'SettingsWorker.handleMessagePort': handleMessagePort,
   'SettingsWorker.validateSetting': validateSetting,
