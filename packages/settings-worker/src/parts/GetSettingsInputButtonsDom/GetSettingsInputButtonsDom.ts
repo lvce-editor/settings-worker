10aa26cf3de02cd33695924eb95051c606f9f483
Le Vivilet <levivilet@pm.me>
2026-05-25 22:50:28 +0200

    fix

--- packages/settings-worker/src/parts/GetSettingsInputButtonsDom/GetSettingsInputButtonsDom.ts
+++
@@ -1,7 +0,0 @@
-import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { getClearButtonDom } from '../GetClearButtonDom/GetClearButtonDom.ts'
-import { getFilterButtonDom } from '../GetFilterButtonDom/GetFilterButtonDom.ts'
-
-export const getSettingsInputButtonsDom = (hasSearchValue: boolean): readonly VirtualDomNode[] => {
-  return [...getClearButtonDom(hasSearchValue), ...getFilterButtonDom()]
-}
--- packages/settings-worker/src/parts/Listen/Listen.ts
+++ packages/settings-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,8 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
-import { registerCommands } from '../SettingsStates/SettingsStates.ts'
 
 export const listen = async (): Promise<void> => {
-  registerCommands(CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
