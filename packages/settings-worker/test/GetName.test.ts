62953a2763b0c07ec0e932d3f9b9c63f0f86f1f4
Le Vivilet <levivilet@pm.me>
2026-05-25 22:48:22 +0200

    test

--- packages/settings-worker/test/GetName.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals'
-import * as GetName from '../src/parts/GetName/GetName.ts'
-
-test('getName returns Settings', () => {
-  const result: string = GetName.getName()
-  expect(result).toBe('Settings')
-})
