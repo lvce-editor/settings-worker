a7fd1ac2698255d4defbe9b75ac7bc217ed4e2f3
Le Vivilet <levivilet@pm.me>
2026-07-19 16:12:51 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,28 @@ import * as config from '@lvce-editor/eslint-config'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   {
     rules: {
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       '@cspell/spellchecker': 'off',
+      'jest/no-disabled-tests': 'off',
+    },
+  },
+  {
+    files: [
+      'packages/settings-worker/src/parts/GetSettingItemsEditor/GetSettingItemsEditor.ts',
+      'packages/settings-worker/src/parts/GetSettingItemsWindow/GetSettingItemsWindow.ts',
+    ],
+    rules: {
+      'virtual-dom/no-object-attribute-values': 'off',
+    },
+  },
+  {
+    files: ['packages/settings-worker/test/**/*.ts'],
+    rules: {
+      'virtual-dom/no-object-attribute-values': 'off',
+      'virtual-dom/prefer-constants': 'off',
     },
   },
 ]
