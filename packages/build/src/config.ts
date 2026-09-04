679d5e265c29927cd73e1bc3d75a209386b7dbe8
Le Vivilet <levivilet@pm.me>
2026-09-04 09:56:49 +0000

    fix: resolve hoisted playwright dependency

--- packages/build/src/config.ts
+++ packages/build/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'settings-worker')
 
 export const workerPath = join(root, '.tmp/dist/dist/settingsWorkerMain.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const playwrightPath = new URL('../../../node_modules/playwright/index.mjs', import.meta.url).toString()
